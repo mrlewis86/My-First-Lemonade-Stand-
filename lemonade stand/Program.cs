30761113b121270d92e5cab4ea9181059a99630e
Mark Lewis <mrlewis86@gmail.com>
2018-07-18 09:47:44 -0500

    Add project files.

---
+++ lemonade stand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lemonade_stand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
