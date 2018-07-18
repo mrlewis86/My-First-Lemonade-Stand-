0831550e4be9d40d2c8d5e258e575b3c3773d324
Mark Lewis <mrlewis86@gmail.com>
2018-07-18 10:26:22 -0500

    brandon lemonade

--- lemonade stand/Program.cs
+++ lemonade stand/Program.cs
@@ -6,7 +6,34 @@ namespace lemonade_stand
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey buddy, nice lemonade stand. Whats the name of it?");
+            Console.WriteLine();
+
+            string userResponse = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("How many cups do you plan to sell?");
+            string NumberOfCups = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Ok, Ok. Well, how much does each cup cost?");
+            int CostOfCups = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Pay me "+ CostOfCups + " for lemonade");
+            Console.WriteLine();
+
+            Console.WriteLine("Well, how much did you pay for each cup?");
+            int PricePerCup = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ill spend " + PricePerCup + "for sugar, water, and crap");
+            Console.WriteLine();
+
+
+
+
+
+
+            Console.ReadLine();
         }
+
+        
     }
 }
