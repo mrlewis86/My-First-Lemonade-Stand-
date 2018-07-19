239f58cb04ac009d6b93c92c8565470cade6f90d
Mark Lewis <mrlewis86@gmail.com>
2018-07-19 09:57:01 -0500

    message

---
+++ lemonade stand/LemonadeStand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lemonade_stand
+{
+    class LemonadeStand
+    {
+        public string Name { get; set; }
+        public int NumberofCups { get; set; }
+        public decimal PricePerCup { get; set; }
+        public decimal CostPerCup { get; set; }
+
+    }
+}
--- lemonade stand/Program.cs
+++ lemonade stand/Program.cs
@@ -6,25 +6,45 @@ namespace lemonade_stand
     {
         static void Main(string[] args)
         {
+            LemonadeStand lemonadeStand = new LemonadeStand();
+
+
             Console.WriteLine("Hey buddy, nice lemonade stand. Whats the name of it?");
             Console.WriteLine();
+            lemonadeStand.Name = Console.ReadLine();
+
 
             string userResponse = Console.ReadLine();
             Console.WriteLine();
 
             Console.WriteLine("How many cups do you plan to sell?");
-            string NumberOfCups = Console.ReadLine();
+            Console.WriteLine();
+            lemonadeStand.NumberofCups = Int32.Parse(Console.ReadLine());
             Console.WriteLine();
 
             Console.WriteLine("Ok, Ok. Well, how much does each cup cost?");
-            int CostOfCups = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Pay me "+ CostOfCups + " for lemonade");
+            Console.WriteLine();
+            lemonadeStand.PricePerCup = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Pay me "+ lemonadeStand.PricePerCup + " for lemonade");
             Console.WriteLine();
 
             Console.WriteLine("Well, how much did you pay for each cup?");
-            int PricePerCup = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Ill spend " + PricePerCup + "for sugar, water, and crap");
             Console.WriteLine();
+            lemonadeStand.CostPerCup = decimal.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Each cup cost me " + lemonadeStand.CostPerCup + " for sugar, water, and crap");
+            Console.WriteLine();
+
+            decimal totalRevenue = lemonadeStand.NumberofCups * lemonadeStand.CostPerCup;
+            decimal totalExpenses = lemonadeStand.PricePerCup * lemonadeStand.CostPerCup;
+            decimal totalProfit = totalRevenue - totalExpenses;
+
+            Console.WriteLine("Your total revenue will be " + totalRevenue);
+            Console.WriteLine("Your total expenses will be " + totalExpenses);
+            Console.WriteLine("Your total profit will be " + totalProfit);
+
+            Console.ReadLine();
+
 
 
 
